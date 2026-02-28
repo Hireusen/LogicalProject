28906cd915eaf47d86ba37f7c3a5de86a68eaa6f
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-02-28 18:51:57 +0900

    initial commit

---
+++ Assets/Racing/Scripts/CarInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 자동차의 정보를 담는 클래스입니다.
+/// </summary>
+public class CarInfo
+{
+    float const Vector2 size = new Vector2(1f, 2f);
+
+    float x;
+    float y;
+    float speed;
+    float angle;
+}
